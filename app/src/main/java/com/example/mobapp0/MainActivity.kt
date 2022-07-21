ea5a50384177a1576cf8660f9a5acf9036d1d269
Okoshuba <kowubao@gmail.com>
2022-07-21 22:33:53 +0300

    Lesson 4.

--- app/src/main/java/com/example/mobapp0/MainActivity.kt
+++ app/src/main/java/com/example/mobapp0/MainActivity.kt
@@ -1,11 +1,30 @@
 package com.example.mobapp0
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun toastMe(view: View){
+        val toast = Toast.makeText(this, "Hello!", Toast.LENGTH_SHORT)
+        toast.show()
+    }
+
+    fun countMe(view: View){
+        val textView: TextView = findViewById(R.id.textView)
+        val countElement = textView.text.toString()
+
+        var count: Int = Integer.parseInt(countElement)
+        count++
+
+        textView.text = count.toString()
     }
 }
